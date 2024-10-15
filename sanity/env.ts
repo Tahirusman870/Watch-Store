19150b6828e980f0bf07c38642a5dbe664bc4f69
Tahir Usman <tahirusmanflancer@gmail.com>
2024-10-15 15:37:36 +0500

    Producion

--- sanity/env.ts
+++ sanity/env.ts
@@ -2,14 +2,16 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-10-08'
 
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  "production",
   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
 )
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  "fbi6v564",
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
+
+export const useCdn = false
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
